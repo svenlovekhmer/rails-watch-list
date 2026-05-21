fffbdfa09cc9560424014831f10d41fa1f82a16d
Alexandre Theol <alexandre.theol@gmail.com>
2026-05-21 10:32:59 +0200

    import config

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,11 @@
+// app/assets/config/manifest.js
+//= link_tree ../images
+//= link_tree ../../javascript .js
+//= link_directory ../stylesheets .css
+
+// app/assets/config/manifest.js
+//= link_tree ../images
+//= link_tree ../../javascript .js
+//= link_directory ../stylesheets .css
+//= link popper.js
+//= link bootstrap.min.js
